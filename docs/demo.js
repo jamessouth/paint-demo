9e507691cc72ec9b81402fe4f061e2f4cbec393b
jamessouth <jamesdanielsouth@gmail.com>
2019-08-03 02:57:08 -0500

    save work

--- docs/demo.js
+++ docs/demo.js
@@ -40,26 +40,37 @@ class Demo {
     return Math.floor(Math.random() * 3) + 1;
   }
 
+  static getColor(base, range) {
+    return base + Math.floor(Math.random() * range + 1);
+  }
+
   paint(ctx, { width, height }, props) { // eslint-disable-line
     // const radgrad = ctx.createRadialGradient(width / 2, height * 1.2, 8, width / 2, height * .9, height);
-    // radgrad.addColorStop(0, `hsla(0deg, 90%, 60%, 80%)`);
+    // radgrad.addColorStop(0, `hsla(40deg, 90%, 60%, 80%)`);
     // // radgrad.addColorStop(0.9, '#ff9F62');
     // radgrad.addColorStop(.55, `hsla(0deg, 80%, 10%, 80%)`);
+    // radgrad.addColorStop(.85, `hsla(0deg, 80%, 0%, 80%)`);
     //
     //
-    // ctx.fillStyle = 'rgb(60, 60, 60)';
-    // ctx.fillRect(0, 0, width, height);
-    for (let i = 0; i < 13000; i += 1) {
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    // ctx.fillStyle = radgrad;
+    ctx.fillRect(0, 0, width, height);
+
+    const ctr = [width/2, height/2 - 5];
+    for (let i = 0; i < 4000; i += 1) {
       const startSide = i % 4;
       const endSide = (startSide + Demo.getEndSide()) % 4;
       ctx.beginPath();
-      ctx.moveTo(...Demo.getPoint(startSide, width, height));
+
+      ctx.moveTo(...ctr);
       ctx.lineTo(...Demo.getPoint(endSide, width, height));
       ctx.lineWidth = Demo.getWidth();
-      // ctx.lineCap = 'square';
-      // ctx.strokeStyle = `hsla(0deg, 50%, 0%, .02)`;
-      ctx.strokeStyle = `rgba(${10 + (i % 20)}, ${5 + (i % 30)}, ${0 + (i % 20)}, .01)`;
+      ctx.lineCap = 'square';
+
+      // ctx.strokeStyle = `rgba(250, 250, 250, .11)`;
+      ctx.strokeStyle = `rgba(${Demo.getColor(240, 15)}, ${Demo.getColor(240, 15)}, ${Demo.getColor(50, 20)}, .05)`;
       ctx.stroke();
+
     }
   }
 }
