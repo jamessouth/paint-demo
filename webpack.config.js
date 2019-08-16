b3587108f9a2ee4ad33cfeac3ba956ab9c71ccee
jamessouth <jamesdanielsouth@gmail.com>
2019-08-16 00:07:02 -0500

    cont demo 3

--- webpack.config.js
+++ webpack.config.js
@@ -94,6 +94,7 @@ module.exports = {
       '!demo2.min.js',
       '!demo3a.min.js',
       '!demo3b.min.js',
+      '!demo3c.min.js',
     ] }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
