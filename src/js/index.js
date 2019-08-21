56255c16b3890c2dc4bde6b716d24d56f64c2f82
jamessouth <jamesdanielsouth@gmail.com>
2019-08-21 12:26:20 -0500

    remove demo 3 from polyfilled imports

--- src/js/index.js
+++ src/js/index.js
@@ -13,9 +13,5 @@ if (CSS.paintWorklet) {
   import(/* webpackChunkName: "css-paint-polyfill" */ 'css-paint-polyfill').then(() => {
     CSS.paintWorklet.addModule('./demo1.min.js');
     CSS.paintWorklet.addModule('./demo2.min.js');
-    CSS.paintWorklet.addModule('./demo3a.min.js');
-    CSS.paintWorklet.addModule('./demo3b.min.js');
-    CSS.paintWorklet.addModule('./demo3c.min.js');
-    CSS.paintWorklet.addModule('./demo3d.min.js');
   });
 }
